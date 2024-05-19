0cb41b33261e9a50fa27397da49f77f9fb7c75b4
Sokamoka <akos.stegner@gmail.com>
2024-05-19 14:19:04 +0200

    Add accordion for ScrollTo promise playground

--- playgrounds/vite/src/pages/scroll-to-promise.vue
+++ playgrounds/vite/src/pages/scroll-to-promise.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue'
 import { useScrollToPromise } from '@lostui/utils'
 import { Button } from '@/components/ui/button'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
 const scrollTo = useScrollToPromise()
 
@@ -11,12 +12,26 @@ async function goTo() {
 }
 
 onMounted(() => goTo())
+
+const accordionItems = [
+  { value: 'item-1', title: 'Is it accessible?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
+  { value: 'item-2', title: 'Is it unstyled?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
+  { value: 'item-3', title: 'Can it be animated?', content: 'Yes! You can use the transition prop to configure the animation.' },
+]
 </script>
 
 <template>
   <Button class="fixed top-2 left-2" @click="goTo">
     GO
   </Button>
+  <Accordion type="single" class="w-full" collapsible :default-value="defaultValue">
+    <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
+      <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+      <AccordionContent>
+        {{ item.content }}
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
   <div class="max-w-4xl p-5 mx-auto space-y-5">
     <div id="first" class="h-screen bg-slate-200">
       A
